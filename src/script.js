fd2821d8cfcf6852c22b7d6336b6811aa12985b7
Young Hyun <naem77716@gmail.com>
2024-09-28 16:24:49 +0900

    이미지 추가

--- src/script.js
+++ src/script.js
@@ -1,9 +1,21 @@
 // 랜더링 될 이미지 집합소
 const images = [
-  "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner.jpg?raw=true",
-  "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner1.jpg?raw=true",
-  "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner2.jpg?raw=true",
-  "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner3.jpg?raw=true"
+  {
+    src: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner.jpg?raw=true",
+    text: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main-text.png?raw=true"
+  },
+  {
+    src: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner1.jpg?raw=true",
+    text: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main-text1.png?raw=true"
+  },
+  {
+    src: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner2.jpg?raw=true",
+    text: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main-text2.png?raw=true"
+  },
+  {
+    src: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner3.jpg?raw=true",
+    text: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main-text3.png?raw=true"
+  },
 ];
 
 // 랜덤 이미지를 선택하자
@@ -15,8 +27,13 @@ function getRandomImage() {
 // 랜덤 이미지를 HTML과 연결 함수
 function setRandomImage() {
   const imgElement = document.getElementById('randomImage');
-  imgElement.src = getRandomImage();
+  const textImageElement = document.getElementById('textImage'); // 설명 이미지를 위한 img 태그
+
+  const randomImage = getRandomImage(); // 랜덤 이미지 객체
+  imgElement.src = randomImage.src; // 랜덤 이미지 src 할당
+  textImageElement.src = randomImage.text; // 설명 이미지 src 할당
 }
+
 
 // 새로고침 할 때 램덤 이미지 함수
 window.onload = function() {
