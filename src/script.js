10867b98c8e5ae46f5707dc78fc42c8bf011aea8
Young Hyun <naem77716@gmail.com>
2024-09-28 21:01:31 +0900

    메인페이지 수정

--- src/script.js
+++ src/script.js
@@ -6,15 +6,15 @@ const images = [
   },
   {
     src: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner1.jpg?raw=true",
-    text: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main-text1.png?raw=true"
-  },
-  {
-    src: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner2.jpg?raw=true",
     text: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main-text2.png?raw=true"
   },
   {
-    src: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner3.jpg?raw=true",
+    src: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner2.jpg?raw=true",
     text: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main-text3.png?raw=true"
+  },
+  {
+    src: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main_banner3.jpg?raw=true",
+    text: "https://github.com/reeyounghyun/k-baemin/blob/main/public/main-text4.png?raw=true"
   },
 ];
 
@@ -27,13 +27,17 @@ function getRandomImage() {
 // 랜덤 이미지를 HTML과 연결 함수
 function setRandomImage() {
   const imgElement = document.getElementById('randomImage');
-  const textImageElement = document.getElementById('textImage'); // 설명 이미지를 위한 img 태그
-
+  const textImageElement = document.getElementById('textImage');
   const randomImage = getRandomImage(); // 랜덤 이미지 객체
-  imgElement.src = randomImage.src; // 랜덤 이미지 src 할당
-  textImageElement.src = randomImage.text; // 설명 이미지 src 할당
+  
+  // 텍스트 이미지 먼저 설정
+  textImageElement.src = randomImage.text;
+  
+  // 텍스트 이미지가 로드된 후 배경 이미지 설정
+  textImageElement.addEventListener('load', function() {
+    imgElement.src = randomImage.src; // 배경 이미지 설정
+  });
 }
-
 
 // 새로고침 할 때 램덤 이미지 함수
 window.onload = function() {
